979ae76b6336ad85178f7fd929c31b77f177b79a
Parthib Deb <debparthib0040@gmail.com>
2024-10-06 00:59:12 +0530

    Add HeroSection Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
+import HeroSection from "./sections/HeroSection";
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
+      <HeroSection />
     </main>
   );
 };
---
+++ src/sections/HeroSection.jsx
@@ -0,0 +1,84 @@
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
+import HackerRoom from "../components/HackerRoom";
+import CanvasLoader from "../components/CanvasLoader";
+// import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants";
+
+const HeroSection = () => {
+  // const controls = useControls("HackerRoom", {
+  //   positionX: {
+  //     value: 2.5,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   positionY: {
+  //     value: 2.5,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   positionZ: {
+  //     value: 2.5,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   rotationX: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   rotationY: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   rotationZ: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   scale: {
+  //     value: 1,
+  //     min: 0.1,
+  //     max: 10,
+  //   },
+  // });
+
+  const isSmall = useMediaQuery({ maxWidth: 320 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col mt-20 sm:mt-36 c-space gap-3">
+        <p className="text-white text-center text-2xl sm:text-3xl font-medium font-generalsans">
+          Hi, my name is Parthib<span className="waving-hand">👋</span>
+        </p>
+        <p className="text-gray_gradient hero_tag">
+          An aspiring Full Stack Developer
+        </p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0">
+        {/* <Leva /> */}
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HackerRoom
+              scale={sizes.deskScale}
+              rotation={[0.3, -Math.PI, 0]}
+              position={sizes.deskPosition}
+            />
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
