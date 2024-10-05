6cc07672bde44d0a56752ad1847a86768d255f74
Parthib Deb <debparthib0040@gmail.com>
2024-10-05 16:44:47 +0530

    Add initial required files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="mx-auto max-w-7xl">
+      <h1 className="text-2xl text-white underline">Hello World</h1>
+    </main>
+  );
+};
+
+export default App;
